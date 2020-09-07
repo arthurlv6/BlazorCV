d35980ae10b60057639fb7d7f17e56fa0f46ebf8
Arthur Lu <arthur.lv6@gmail.com>
2020-09-08 11:48:47 +1200

    Add project files.

---
+++ BlazorCV/Components/DetailBase.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCV.Components
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Type { get; set; } = "text";
+        public bool Show { get; set; } = false;
+    }
+    public class DetailBase: ComponentBase
+    {
+        public List<Item> Items { get; set; } = new List<Item>();
+        public string Height { get; set; }
+        public int Id { get; set; }
+        public bool ShowDialog { get; set; }
+        public void Show(string name)
+        {
+            Items.RemoveAll(d=>true);
+            Items.Add(new Item { Name = "certificates", Type = "notText" });
+            Items.Add(new Item { Name = "2018" });
+            Items.Add(new Item { Name = "2016" });
+            Items.Add(new Item { Name = "2014.2" });
+            Items.Add(new Item { Name = "2014.1" });
+            Items.Add(new Item { Name = "2013"  });
+            Items.Add(new Item { Name = "2012"  });
+
+            Items.Add(new Item { Name = "stock",Type="notText" });
+            var temp = Items;
+            var item = Items.Find(d => d.Name == name);
+
+            item.Show = true;
+            Height = item.Type == "notText" ? "800px" : "400px";
+            ShowDialog = true;
+            StateHasChanged();
+        }
+        public void Close()
+        {
+            ShowDialog = false;
+        }
+    }
+}
---
+++ BlazorCV/Pages/IndexBase.cs
@@ -0,0 +1,19 @@
+﻿using BlazorCV.Components;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCV.Pages
+{
+    public class IndexBase:ComponentBase
+    {
+        public Detail Detail { get; set; }
+        public void ModalShow(string name)
+        {
+            Detail.Show(name);
+        }
+        
+    }
+}
